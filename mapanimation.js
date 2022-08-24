56436678f52c44cf6d18f3ec6bb00b9c81c17142
JEAN PAUL JOVIN <107082551+jovinjp@users.noreply.github.com>
2022-08-24 09:24:02 -0400

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -1,41 +1,43 @@
 const busStops = [
-  [-71.093729, 42.359244],
-  [-71.094915, 42.360175],
-  [-71.0958, 42.360698],
-  [-71.099558, 42.362953],
-  [-71.103476, 42.365248],
-  [-71.106067, 42.366806],
-  [-71.108717, 42.368355],
-  [-71.110799, 42.369192],
-  [-71.113095, 42.370218],
-  [-71.115476, 42.372085],
-  [-71.117585, 42.373016],
-  [-71.118625, 42.374863],
+  [-71.08430208, 42.34222241],
+  [-71.08373, 42.32988],
+  [-71.09190328, 42.35569503],
+  [-71.11634544, 42.37119303],
+  [-71.08412507, 42.34226185],
+  [-71.08373, 42.32988],
+  [-71.11746, 42.37307],
+  [-71.09240551, 42.35688732],
+  [-71.11631221, 42.3711808],
+  [-71.08295033, 42.34138113],
+  [-71.08373, 42.32988],
+  [-71.11746, 42.37307],
+  [-71.09246289, 42.35698517],
+  [-71.11479952, 42.370650779],
+  
   ];
 
+//
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2xhZG9nZW5lc2lzIiwiYSI6ImNsNm4wNTM0ZDBxYzQzZG96a3J1eGxseGYifQ.ARomU9hs2fIlX7P0RdiE7w';
 
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-71.104081, 42.365554],
+    center: [-71.09240551, 42.35688732],
     zoom: 14,
   });
   
-  var marker = new mapboxgl.Marker({ color: "#82ABE2", scale: 1 })
-    .setLngLat([-71.093729, 42.359244]) 
+  var marker = new mapboxgl.Marker({ color: "#82ABE2", scale: 1})
+    .setLngLat([-71.08430208, 42.34222241])
     .addTo(map);
   
   let counter = 0;
   function move() {
+    
     setTimeout(() =>{
-      if (counter >= bustops.length) return;
+
+      if (counter >= busStops.length) return;
       marker.setLngLat(busStops[counter]);
       counter++;
       move();
     }, 1000);
-  }
-  
-  if (typeof module !== 'undefined') {
-    module.exports = { move };
   }
